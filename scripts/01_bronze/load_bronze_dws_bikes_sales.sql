7ec1f45aa3d971a4655ae3be65f27fe4532689c4
Mogheer Eid <mogheereid4@gmail.com>
2025-03-04 09:38:14 -0300

    Create load_bronze_dws_bikes_sales.sql

---
+++ scripts/01_bronze/load_bronze_dws_bikes_sales.sql
@@ -0,0 +1,197 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC bronze.load_bronze;
+===============================================================================
+*/
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
+	BEGIN TRY
+		SET @batch_start_time = GETDATE();
+		PRINT '================================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '================================================';
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.addresses';
+		TRUNCATE TABLE bronze.addresses;
+		PRINT '>> Inserting Data Into: bronze.addresses';
+		BULK INSERT bronze.addresses
+		FROM 'C:\Users\mogheer.eid\OneDrive - Accenture\Visual Team\Usecase\04_SQL\01_MyProjects\01_DW_BikeSales\01_Dataset\Addresses.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.business_partners';
+		TRUNCATE TABLE bronze.business_partners;
+		PRINT '>> Inserting Data Into: bronze.business_partners';
+		BULK INSERT bronze.business_partners
+		FROM 'C:\Users\mogheer.eid\OneDrive - Accenture\Visual Team\Usecase\04_SQL\01_MyProjects\01_DW_BikeSales\01_Dataset\BusinessPartners.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.employees';
+		TRUNCATE TABLE bronze.employees;
+		PRINT '>> Inserting Data Into: bronze.employees';
+		BULK INSERT bronze.employees
+		FROM 'C:\Users\mogheer.eid\OneDrive - Accenture\Visual Team\Usecase\04_SQL\01_MyProjects\01_DW_BikeSales\01_Dataset\Employees.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.product_categories';
+		TRUNCATE TABLE bronze.product_categories;
+		PRINT '>> Inserting Data Into: bronze.product_categories';
+		BULK INSERT bronze.product_categories
+		FROM 'C:\Users\mogheer.eid\OneDrive - Accenture\Visual Team\Usecase\04_SQL\01_MyProjects\01_DW_BikeSales\01_Dataset\ProductCategories.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.product_categories_text';
+		TRUNCATE TABLE bronze.product_categories_text;
+		PRINT '>> Inserting Data Into: bronze.product_categories_text';
+		BULK INSERT bronze.product_categories_text
+		FROM 'C:\Users\mogheer.eid\OneDrive - Accenture\Visual Team\Usecase\04_SQL\01_MyProjects\01_DW_BikeSales\01_Dataset\ProductCategoryText.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.products';
+		TRUNCATE TABLE bronze.products;
+		PRINT '>> Inserting Data Into: bronze.products';
+		BULK INSERT bronze.products
+		FROM 'C:\Users\mogheer.eid\OneDrive - Accenture\Visual Team\Usecase\04_SQL\01_MyProjects\01_DW_BikeSales\01_Dataset\Products.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.products_text';
+		TRUNCATE TABLE bronze.products_text;
+		PRINT '>> Inserting Data Into: bronze.products_text';
+		BULK INSERT bronze.products_text
+		FROM 'C:\Users\mogheer.eid\OneDrive - Accenture\Visual Team\Usecase\04_SQL\01_MyProjects\01_DW_BikeSales\01_Dataset\ProductTexts.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+		
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.sales_order_items';
+		TRUNCATE TABLE bronze.sales_order_items;
+		PRINT '>> Inserting Data Into: bronze.sales_order_items';
+		BULK INSERT bronze.sales_order_items
+		FROM 'C:\Users\mogheer.eid\OneDrive - Accenture\Visual Team\Usecase\04_SQL\01_MyProjects\01_DW_BikeSales\01_Dataset\SalesOrderItems.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.sales_orders';
+		TRUNCATE TABLE bronze.sales_orders;
+		PRINT '>> Inserting Data Into: bronze.sales_orders';
+		BULK INSERT bronze.sales_orders
+		FROM 'C:\Users\mogheer.eid\OneDrive - Accenture\Visual Team\Usecase\04_SQL\01_MyProjects\01_DW_BikeSales\01_Dataset\SalesOrders.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+		PRINT '======================================================================================================='
+
+
+		SET @batch_end_time = GETDATE();
+		PRINT '=========================================='
+		PRINT 'Loading Bronze Layer is Completed';
+        PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '=========================================='
+	END TRY
+	BEGIN CATCH
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+	END CATCH
+END
+
+
+
+----------
+
+--SELECT * FROM bronze.crm_cust_info
+
+--EXEC bronze.load_bronze
